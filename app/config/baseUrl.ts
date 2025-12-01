af75af35a79651625b67cafe7769c2fd21bf7110
joselivia <josephmutunga39@gmail.com>
2025-12-01 12:06:34 +0300

    url

--- app/config/baseUrl.ts
+++ app/config/baseUrl.ts
@@ -1,1 +1,2 @@
-export const baseURL = "http://localhost:8082";
+//export const baseURL = "http://localhost:8082";
+export const baseURL="https://swisspollint.com";
--- app/page.tsx
+++ app/page.tsx
@@ -620,9 +620,9 @@ export default function HomePage() {
                               cx="50%"
                               cy="50%"
                               labelLine={false}
-                              label={({ name, percent }) =>
-                                `${name} ${(percent * 100).toFixed(0)}%`
-                              }
+                        label={({ name, percent }) => {
+  return `${name} ${percent ? (percent * 100).toFixed(0) : ''}%`;
+}}
                               outerRadius={100}
                               fill="#8884d8"
                               dataKey="value"
